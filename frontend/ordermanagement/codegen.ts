ec554411eebf007adc053dd6cbb44fd13e56aa25
taesik mun <taesic12@knou.ac.kr>
2023-07-30 00:16:40 +0900

    codegen making gql file and schema.ts

---
+++ frontend/ordermanagement/codegen.ts
@@ -0,0 +1,16 @@
+
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "http://localhost:5157/graphql/",
+  documents: "**/*.{gql,graphql}",
+  generates: {
+    'src/graphql/generated/schema.ts': {
+      plugins: ['typescript', 'typescript-operations', 'typescript-react-apollo'],
+      
+    }
+  }
+};
+
+export default config;
